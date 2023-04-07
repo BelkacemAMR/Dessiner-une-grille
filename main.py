b6fb4a76cc088af9083ed6fca54f204881adc60f
Belkacem <abel-and-co@hotmail.com>
2023-04-07 08:39:49 +0200

    Version Finale

---
+++ main.py
@@ -0,0 +1,24 @@
+def draw_n_squares(n):
+    if n % 2 == 1:
+        v = n * 2 + 1
+        h = v + (n * 2)
+
+    result = ""
+
+    for i in range(v):
+        if i % 2 == 0:
+            for j in range(h):
+                if j % 4 == 0:
+                    result += "+"
+                else:
+                    result += "-"
+        else:
+            for j in range(h):
+                if j % 4 == 0:
+                    result += "|"
+                else:
+                    result += " "
+
+        result += "\n"
+
+    return result
